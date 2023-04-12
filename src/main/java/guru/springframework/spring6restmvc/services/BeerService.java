f937c50cb086f729b8bc3d750b5fbe9bdd9da439
Bruno Pugliese <bruno.orsolon@gmail.com>
2023-04-12 12:11:09 -0300

    Beer services

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -4,9 +4,6 @@ import guru.springframework.spring6restmvc.model.Beer;
 
 import java.util.UUID;
 
-/**
- * Created by jt, Spring Framework Guru.
- */
 public interface BeerService {
 
     Beer getBeerById(UUID id);
