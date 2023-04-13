30a1b1cb6f5dcdebd9a79c7d0bcf704596247805
Bruno Pugliese <bruno.orsolon@gmail.com>
2023-04-13 19:13:12 -0300

    Spring MVC Rest Services

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -1,12 +1,21 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
 
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
+
+    Beer saveNewBeer(Beer beer);
+
+    void updateBeerById(UUID beerId, Beer beer);
+
+    void deleteById(UUID beerId);
+
+    void patchBeerById(UUID beerId, Beer beer);
 }
