c992b562904fa71c3f8b755ed804a5e8607b600e
Bruno Pugliese <bruno.orsolon@gmail.com>
2023-04-13 15:58:49 -0300

    Beer controller and tests

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
